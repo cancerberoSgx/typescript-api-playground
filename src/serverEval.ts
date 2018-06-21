fc0705709040d635a0c99ad66efe7bc97281065f
Sebastián Gurin <sebastigurin@gmail.com>
2018-06-21 16:25:33 -0300

    ts-transformation-2 to file

--- src/serverEval.ts
+++ src/serverEval.ts
@@ -198,80 +198,11 @@ const transformer = <T extends ts.Node>(context: ts.TransformationContext) => {
 }
 const value = new Foo().magic2(1 + 2 + 3)`,
     
-    codeValue: `// this example will create a simple typescript source file programmatically, parse it to AST Nodes and then 
-// use TypeScript Transformations to manipulate some ot its nodes (changing  particular arithmetic expressions). 
-// Finally , the transformed AST will be printed back to another source file as a string
-
-// (Note: I've taken this example from somewhere else some credits are not mine - but since there's limited typescript 
-// documentation I think is a good idea to duplicate this... )
-
-import * as ts from 'typescript'
-
-export function main(source: string, log: (msg: string) => void): string {
-
-  const sourceFile: ts.SourceFile = ts.createSourceFile(
-    'test.ts', source, ts.ScriptTarget.ES2015, true, ts.ScriptKind.TS
-  )
-
-  // Apply transformation to the sourcefile 
-  const result: ts.TransformationResult<ts.SourceFile> = ts.transform<ts.SourceFile>(
-    sourceFile, [transformer]
-  )
-  // obtain the transformed source file
-  const transformedSourceFile: ts.SourceFile = result.transformed[0]
-  const printer: ts.Printer = ts.createPrinter()
-
-  const transformedContent = printer.printFile(transformedSourceFile)
-
-  log('Original file:\\n' + printer.printFile(sourceFile) + 'Transformed file:\\n' + transformedContent)
-
-  result.dispose()
-  return transformedContent
-}
-
-// Now the interesting part, we will build a Transformation that will replace particular nodes of 
-// the AST, BinaryExpressions like a+b, and replace them with with the number literal resulting 
-// of applying the mathematical operation. In this case the expression "1 + 1 + 3" will be replaced 
-// with the expression "6"
-const transformer = <T extends ts.Node>(context: ts.TransformationContext) => (rootNode: T) => {
-
-  // visit() function will visit all the descendants node (recursively)  
-  function visit(node: ts.Node): ts.Node {
-    node = ts.visitEachChild(node, visit, context)
-
-    // Here we filter which node we want to manipulate / replace, in our case binary expressions
-    if (ts.isBinaryExpression(node)) {
-
-      if (node.left.kind === ts.SyntaxKind.NumericLiteral &&
-        node.right.kind === ts.SyntaxKind.NumericLiteral) {
-        const left = node.left as ts.NumericLiteral
-        const leftVal = parseFloat(left.text)
-        const right = node.right as ts.NumericLiteral
-        const rightVal = parseFloat(right.text)
-        switch (node.operatorToken.kind) {
-          case ts.SyntaxKind.PlusToken:
-
-            // Important, returning another node will replace the original "node" with returned one. 
-            // In our case we will be replacing binary expressions like 1.2 with the actual result of 
-            // the operaton (as long as operands are literals)
-            return ts.createLiteral(leftVal + rightVal)
-          case ts.SyntaxKind.AsteriskToken:
-            return ts.createLiteral(leftVal * rightVal)
-          case ts.SyntaxKind.MinusToken:
-            return ts.createLiteral(leftVal - rightVal)
-        }
-      }
-    }
-    // for all the other kind of nodes, we just return the node (no transformation or replacement)
-    return node
-  }
-  return ts.visitNode(rootNode, visit)
-}
-`
+    codeValue: readFileSync('./assets/examples/ts-transformation-2/code.ts').toString()
   }, 
   
+
   {
-  
     name: 'Transformation 3', 
     description: 'Another Transformation API example that will add new nodes, this time putting a name to functions declared without name',
     replaceInputEditorContentWithReturnValue: true,
@@ -288,9 +219,7 @@ const alsoWithName = function(){
 (function (a: number) { return a + 1; })(5); // and with this one
 `,
   codeValue: `import * as ts from 'typescript';
-
 // since having function declarations without name is an error in TypeScript this transformation will put them name
-
 function main(source: string, log: (m: string)=>void):string {
   let nameCounter = 0
   const transformFactory = (context: ts.TransformationContext) => (rootNode: ts.SourceFile): ts.SourceFile => {
@@ -312,7 +241,6 @@ function main(source: string, log: (m: string)=>void):string {
     }
     return ts.visitNode(rootNode, visit)
   }
-
   const sourceFile = ts.createSourceFile(
     'test.ts', source, ts.ScriptTarget.ES2015, true, ts.ScriptKind.TS
   )
